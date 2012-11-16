0ae0266406365ded44b927fcb4c02ac0372db8d3
Humaira Tasnim <humaira.cmu@gmail.com>
2012-11-16 23:57:39 +0300

    conference views edited

--- app/models/conference.rb
+++ app/models/conference.rb
@@ -1,3 +1,7 @@
 class Conference < ActiveRecord::Base
 	has_many :committees
+
+	def duration
+      "#{start_date.strftime('%b %d, %Y')} - #{end_date.strftime('%b %d, %Y')}"
+    end
 end
--- app/models/delegate.rb
+++ app/models/delegate.rb
@@ -1,3 +1,7 @@
 class Delegate < ActiveRecord::Base
 	belongs_to :school
+
+	def delegate_name
+      "#{first_name} #{last_name}"
+    end
 end
