9bc4100491abaf73b8753fb92a230b5e0275cb01
Humaira Tasnim <humaira.cmu@gmail.com>
2012-12-02 21:06:10 +0300

    all email bodies complete

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,17 +4,17 @@ class UserMailer < ActionMailer::Base
 
   def new_school_msg(school)
     @school = school
-    mail(:to => school.email, :subject => "Conference Participation Confirmation")
+    mail(:to => school.user.email, :subject => "Request for Conference Registration received")
   end
 
   def update_delegation_assignment_msg(delegation_assignment)
     @delegation_assignment = delegation_assignment
-    mail(:to => delegation_assignment.school.email, :subject => "Countries have been assigned to your school")
+    mail(:to => delegation_assignment.school.email, :subject => "Congrats! Your school has been accepted")
   end
 
   def new_conference_msg(conference)
     @conference = conference
-    mail(:to => conference.user.email, :subject => "You have been assigned a conference")
+    mail(:to => conference.user.email, :subject => "You have been assigned as a Conference Manager")
   end
 
 end
