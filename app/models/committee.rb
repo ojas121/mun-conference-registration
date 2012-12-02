196ae03f109c5719173788a9d6601eea4c406142
Wadha Alajmy <walajmy@gmail.com>
2012-12-02 11:03:29 +0300

    Changes in UI

--- app/models/committee.rb
+++ app/models/committee.rb
@@ -2,6 +2,12 @@ class Committee < ActiveRecord::Base
 	belongs_to :conference
 	belongs_to :list
 
+
+	validates :committee_name, :presence => true,
+                      :length => { :minimum => 2 }
+
+	validates :num_delegates, :presence => true
+
 	has_many :committee_schools
 	has_many :schools, :through => :committee_schools
 
--- app/models/conference.rb
+++ app/models/conference.rb
@@ -8,6 +8,30 @@ class Conference < ActiveRecord::Base
 	geocoded_by :venue
 	after_validation :geocode, :if => :venue_changed?
 
+
+  	validates :conference_title, :presence => true,
+                      :length => { :minimum => 3 }
+    validates_presence_of :host
+    validates_presence_of :venue
+    validates_presence_of :conference_details
+    validates_presence_of :start_date
+    validates_presence_of :end_date
+    validate :start_must_be_before_end_date
+    validate :start_date_not_in_past
+
+
+def start_must_be_before_end_date
+  errors.add(:start_date, "Must be before end time") unless 
+  self.start_date < self.end_date
+end
+
+
+def start_date_not_in_past
+  errors.add(:start_date, "must not be in past") unless 
+  self.start_date > Date.current
+end 
+
+
 	def duration
       "#{start_date.strftime('%b %d, %Y')} - #{end_date.strftime('%b %d, %Y')}"
     end
